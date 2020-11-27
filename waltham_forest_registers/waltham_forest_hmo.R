a3964f9e7d6c9bfb7dfae5146de8efaf046e91b4
makaibe <nibe0993@gmail.com>
2020-11-27 23:59:22 +0000

    Filtered on specific stations

--- waltham_forest_registers/waltham_forest_hmo.R
+++ waltham_forest_registers/waltham_forest_hmo.R
@@ -19,3 +19,17 @@ wf_geo_df <- create_geo_mapped_df(df, "post_code") %>%
                        , distance_in_km
                        , distance_in_miles
                        )
+
+wf_geo_df$nearest_station <- trimws(wf_geo_df$nearest_station)
+
+wf_geo_df %>% 
+  group_by(nearest_station) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n))
+
+stations_to_keep <-  c("Wood Street", "Walthamstow Central", "St James Street")
+
+write.csv(wf_geo_df[wf_geo_df$nearest_station %in% stations_to_keep,]
+          , "~/Documents/gh-repos/project-portfolio/waltham_forest_registers/20201127_hmo_reg_geo_mapped.csv"
+          , row.names = F )
+
