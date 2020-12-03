49cc7623dc053d80448c44533cb5dff87730f78b
makaibe <nibe0993@gmail.com>
2020-12-03 09:54:52 +0000

    Added correct solution

--- 100_days_of_code/day_02/2_3/main.py
+++ 100_days_of_code/day_02/2_3/main.py
@@ -1,8 +1,15 @@
 #  Don't change the code below 
-age = input("What is your current age?")
+age = input("What is your current age? ")
 #  Don't change the code above 
 
 #Write your code below this line 
+years_left = 90 - int(age)
+
+d = years_left * 365
+w = years_left * 52
+m = years_left * 12
+
+print(f"You have {d} days, {w} weeks, and {m} months left.")
 
 
 
