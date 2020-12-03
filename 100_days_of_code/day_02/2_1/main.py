0eeed69fe4c722b799b68acf46a7145312229fed
makaibe <nibe0993@gmail.com>
2020-12-03 06:40:46 +0000

    Added first attempt

--- 100_days_of_code/day_02/2_1/main.py
+++ 100_days_of_code/day_02/2_1/main.py
@@ -5,8 +5,11 @@ two_digit_number = input("Type a two digit number: ")
 ####################################
 #Write your code below this line 👇
 
+num_input = input("Type a two digit number: ")
 
+to_int_num_input = int(num_input)
 
+print(to_int_num_input[0]+to_int_num_input[1])
 
 
 
