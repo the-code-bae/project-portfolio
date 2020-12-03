d94b422daaa254093941c273fd35aa63cae988ba
makaibe <nibe0993@gmail.com>
2020-12-03 06:58:26 +0000

    Added exercise file

---
+++ 100_days_of_code/day_02/2_2/main.py
@@ -0,0 +1,16 @@
+#  Don't change the code below 
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+#  Don't change the code above 
+
+#Write your code below this line 
+
+
+
+
+
+
+
+
+
+
