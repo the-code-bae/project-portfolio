cf406aa06b53bf97ed600552486f68ebb63c1165
makaibe <nibe0993@gmail.com>
2020-12-03 06:45:50 +0000

    Remove emojis and removed duplication

--- 100_days_of_code/day_02/2_1/main.py
+++ 100_days_of_code/day_02/2_1/main.py
@@ -1,13 +1,11 @@
-# 🚨 Don't change the code below 👇
+#  Don't change the code below 
 two_digit_number = input("Type a two digit number: ")
-# 🚨 Don't change the code above 👆
+#  Don't change the code above 
 
 ####################################
-#Write your code below this line 👇
+#Write your code below this line 
 
-num_input = input("Type a two digit number: ")
-
-to_int_num_input = int(num_input)
+to_int_num_input = int(two_digit_number)
 
 print(to_int_num_input[0]+to_int_num_input[1])
 
