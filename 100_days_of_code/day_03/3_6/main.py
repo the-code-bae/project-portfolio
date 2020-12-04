dfac3d33426242425fce7286abd9ba1d5f68cc7a
makaibe <nibe0993@gmail.com>
2020-12-04 20:37:02 +0000

    Final solution

--- 100_days_of_code/day_03/3_6/main.py
+++ 100_days_of_code/day_03/3_6/main.py
@@ -25,19 +25,37 @@ print("Your mission is to find the treasure.")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-direction = input("You're at a cross road. Where do you want to go? Type 'left' or 'right' \n")
-sink_or_swim = input("You've come to a lake. There is an island in the middle of the lake. Type ""wait"" to wait for a boat. Type ""swim"" to swim across. \n")
-which_door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n")
+direction = input("You're at a cross road. Where do you want to go? Type 'left' or 'right' \n").lower()
+# sink_or_swim = input("You've come to a lake. There is an island in the middle of the lake. Type ""wait"" to wait for a boat. Type ""swim"" to swim across. \n").lower()
+# which_door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
 
-if direction.lower() != "left":
-	print("Fall into a hole. Game Over.")
-elif sink_or_swim.lower() != "wait":
-	print("Attacked by trout. Game Over.")
-elif which_door.lower() == "blue":
-	print("Eaten by beasts. Game Over")
-elif which_door.lower() == "red":
-	print("Burned by fire. Game Over")
-elif which_door.lower() == "yellow":
-	print("You Win!")
+if direction == "left":
+	sink_or_swim = input("You've come to a lake. There is an island in the middle of the lake. Type ""wait"" to wait for a boat. Type ""swim"" to swim across. \n").lower()
+	if sink_or_swim == "wait":
+		which_door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+		if which_door == "blue":
+			print("Eaten by beasts. Game Over")
+		elif which_door.lower() == "red":
+			print("Burned by fire. Game Over")
+		elif which_door.lower() == "yellow":
+			print("You Win!")
+		else:
+			print("Game Over")
+	else:
+		print("Attacked by trout. Game Over.")
 else:
-	print("Game Over")
+	print("Fall into a hole. Game Over.")
+
+
+# if direction.lower() != "left":
+# 	print("Fall into a hole. Game Over.")
+# elif sink_or_swim.lower() != "wait":
+# 	print("Attacked by trout. Game Over.")
+# elif which_door.lower() == "blue":
+# 	print("Eaten by beasts. Game Over")
+# elif which_door.lower() == "red":
+# 	print("Burned by fire. Game Over")
+# elif which_door.lower() == "yellow":
+# 	print("You Win!")
+# else:
+# 	print("Game Over")
