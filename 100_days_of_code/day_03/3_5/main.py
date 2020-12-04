e92ab15283617db06ec760109a5977d22b47d4de
makaibe <nibe0993@gmail.com>
2020-12-04 19:24:17 +0000

    Amend if statement

--- 100_days_of_code/day_03/3_5/main.py
+++ 100_days_of_code/day_03/3_5/main.py
@@ -34,7 +34,7 @@ final_score = int(str(true_count) + str(love_count))
 
 if final_score < 10 or final_score > 90:
 	print(f"Your score is {final_score}, you go together like coke and mentos.")
-elif final_score > 40 and final_score < 50:
+elif final_score >= 40 and final_score =< 50:
 	print(f"Your score is {final_score}, you are alright together.")
 else:
 	print(f"Your score is {final_score}.")
