6e25467c1a2c91b4b9ead6cc3347b26cb701371d
Maka <nibe0993@gmail.com>
2020-12-04 11:37:37 +0000

    Added solution

--- 100_days_of_code/day_03/3_1/main.py
+++ 100_days_of_code/day_03/3_1/main.py
@@ -4,6 +4,10 @@ number = int(input("Which number do you want to check? "))
 
 #Write your code below this line 
 
+if number % 2 == 0:
+	print("This is an even number.")
+else:
+	print("This is an odd number.")
 
 
 
