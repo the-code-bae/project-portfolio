6ef3b5fb06f8e9219dfcbb227e3f9164eb508ce3
Maka <nibe0993@gmail.com>
2020-12-04 12:39:33 +0000

    First attempt

---
+++ 100_days_of_code/day_03/3_3/main.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if year % 4 == 0 & year % 100 == 0:
+ 	print(f"The year {year} is a leap year")
+elif year % 400 == 0:
+	print(f"The year {year} is a leap year")
+elif year % 4 == 0:
+	print(f"The year {year} is a leap year")
+else:
+	print(f"The year {year} is not a leap year")
+
