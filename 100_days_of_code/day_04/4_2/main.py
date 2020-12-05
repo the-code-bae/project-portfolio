cc51e499102a1360e10e6f518ac2afe3be5bc84c
makaibe <nibe0993@gmail.com>
2020-12-05 15:18:26 +0000

    Added solution

---
+++ 100_days_of_code/day_04/4_2/main.py
@@ -0,0 +1,14 @@
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+pick_name = random.randint(0, len(names))
+
+payer = names[pick_name]
+
+print(f"{payer} is going to buy the meal today!")
