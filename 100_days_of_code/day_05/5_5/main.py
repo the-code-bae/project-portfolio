933b77ab1ad3d522a85a977a953565e1c4f0773f
makaibe <nibe0993@gmail.com>
2020-12-07 10:14:40 +0000

    Final solution
    Refactored the code and optimized it after watching video. Previous solution included a major flaw, which would only give you a letter based on a range from a sample which would never retrieve any random letters, symbols, numbers beyond x in the list where x is the number chosen by the user.

--- 100_days_of_code/day_05/5_5/main.py
+++ 100_days_of_code/day_05/5_5/main.py
@@ -13,48 +13,49 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 # Create random list of numbers to each component of the password
-random_letters = random.sample(range(0,nr_letters),nr_letters)
-random_symbols = random.sample(range(0,nr_symbols),nr_symbols)
-random_numbers = random.sample(range(0,nr_numbers),nr_numbers)
-
-print(random_letters)
-print(len(random_letters))
-
-# print(random_symbols)
-# print(len(random_symbols))
-
-# print(random_numbers)
-# print(len(random_numbers))
-
-letter_section = ''
-
-for num in random_letters:
-	letter_section += letters[num]
-
-symbol_section = ''
-
-for num in random_symbols:
-	symbol_section += symbols[num]
-
-number_section = ''
-
-for num in random_numbers:
-	number_section += numbers[num]	
+# random_letters = random.sample(range(0,nr_letters),nr_letters)
+# random_symbols = random.sample(range(0,nr_symbols),nr_symbols)
+# random_numbers = random.sample(range(0,nr_numbers),nr_numbers)
 
 
-final_output = letter_section + symbol_section + number_section
+password = ''
 
-print(final_output)
+for num in range(0, nr_letters):
+	password += random.choice(letters)
 
+for num in range(0, nr_symbols):
+	password += random.choice(symbols)
+
+for num in range(0, nr_numbers):
+	password += random.choice(numbers)
+
+print(password)
+print(len(password))
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
-randomise_final = random.sample(range(0, len(final_output)),len(final_output))
 
-hard_final_output = ''
+password_list = []
 
-for num in randomise_final:
-	hard_final_output += final_output[num]
+for num in range(0, nr_letters):
+	password_list.append(random.choice(letters))
 
-print(hard_final_output)
+for num in range(0, nr_symbols):
+	password_list.append(random.choice(symbols))
+
+for num in range(0, nr_numbers):
+	password_list.append(random.choice(numbers))
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+print(len(password_list))
+
+final_password = ""
+
+for char in password_list:
+	final_password += char
+
+print(final_password)
