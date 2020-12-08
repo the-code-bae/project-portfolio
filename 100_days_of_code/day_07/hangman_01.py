291b360e72c37b4a72269ab9395228641783cfe8
makaibe <nibe0993@gmail.com>
2020-12-08 06:35:58 +0000

    Changed variable names and make input string lowercase

--- 100_days_of_code/day_07/hangman_01.py
+++ 100_days_of_code/day_07/hangman_01.py
@@ -5,16 +5,16 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-guess_word = random.choice(word_list)
+chosen_word = random.choice(word_list)
 # print(guess_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess a letter: ")
+guess = input("Guess a letter: ").lower()
 
 # #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-for letter in guess_word:
+for letter in chosen_word:
 	if letter == guess:
 		print("Right")
 	else:
