2bce90102cc2f8f6d93222c9d877233d2d94b80a
makaibe <nibe0993@gmail.com>
2020-12-09 08:28:53 +0000

    Added solution

--- 100_days_of_code/day_08/8_1.py
+++ 100_days_of_code/day_08/8_1.py
@@ -1,9 +1,10 @@
 #Write your code below this line 👇
 
+import math 
 
-
-
-
+def paint_calc(height, width, cover):
+	number_of_cans = math.ceil((height * width)/cover)
+	print(f"You\'ll need {number_of_cans} cans of paint")
 
 
 #Write your code above this line 👆
