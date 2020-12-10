89048e837bbba3dc6cfd152826e35a0cd72a0979
makaibe <nibe0993@gmail.com>
2020-12-10 07:45:51 +0000

    Deleted redundant comments

--- 100_days_of_code/day_08/8_5.py
+++ 100_days_of_code/day_08/8_5.py
@@ -22,27 +22,6 @@ def caesar(plain_text, shift_amount, cipher_direction):
     cipher_text += alphabet[new_position]
   print(f"The {cipher_direction} text is {cipher_text}")
 
-# def encrypt(plain_text, shift_amount):
-#   cipher_text = ""
-#   for letter in plain_text:
-#     position = alphabet.index(letter)
-#     new_position = position + shift_amount
-#     cipher_text += alphabet[new_position]
-#   print(f"The encoded text is {cipher_text}")
-
-# def decrypt(cipher_text, shift_amount):
-#   plain_text = ""
-#   for letter in cipher_text:
-#     position = alphabet.index(letter)
-#     new_position = position - shift_amount
-#     plain_text += alphabet[new_position]
-#   print(f"The decoded text is {plain_text}")
-
-# if direction == "encode":
-#   encrypt(plain_text=text, shift_amount=shift)
-# elif direction == "decode":
-#   decrypt(cipher_text=text, shift_amount=shift)
-
 #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
 
 caesar(text, shift, direction)
