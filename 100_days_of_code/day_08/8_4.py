781ae62090225ad8114511b8dc6d418f75248048
makaibe <nibe0993@gmail.com>
2020-12-10 07:17:25 +0000

    Added decrypt function

--- 100_days_of_code/day_08/8_4.py
+++ 100_days_of_code/day_08/8_4.py
@@ -14,6 +14,14 @@ def encrypt(plain_text, shift_amount):
 
 #TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
+def decrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    cipher_text += alphabet[new_position]
+  print(f"The encoded text is {cipher_text}")
+
   #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
   #e.g. 
   #cipher_text = "mjqqt"
@@ -23,4 +31,5 @@ def encrypt(plain_text, shift_amount):
 
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
+# encrypt(plain_text=text, shift_amount=shift)
+decrypt(plain_text=text, shift_amount=shift)
