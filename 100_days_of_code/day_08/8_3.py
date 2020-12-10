1f7cae74c7dd54834dca1fcdc9c9349ac21369c2
makaibe <nibe0993@gmail.com>
2020-12-10 07:02:51 +0000

    First attempt

--- 100_days_of_code/day_08/8_3.py
+++ 100_days_of_code/day_08/8_3.py
@@ -1,30 +1,17 @@
+# from collections import deque
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-print(alphabet[0+5])
-test = "hello"
-test2 = [i for i in test]
-# print(test2[1])
-# test2[1] = "a"
-# print(test2)
-for i in range(0,len(test2)):
-    test2[i] = alphabet[i-5]
-print(test2)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-# for i in test:
-    
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-# text = input("Type your message:\n").lower()
-# shift = int(input("Type the shift number:\n"))
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
 
-# #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-# def encrypt(text, shift):
-# 	# output = ''
-# 	test = [i for i in text]
-# 	for letter in test:
-# 		test[test.index(letter)] = alphabet[test.index(letter)+shift]
-
-# 	print(test.join())
+    # turn text into string
+    text_as_list = [i for i in text]
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
@@ -32,6 +19,12 @@ print(test2)
     #shift = 5
     #cipher_text = "mjqqt"
     #print output: "The encoded text is mjqqt"
+    
+    for i in range(0,len(text_as_list)):
+        original_position = alphabet.index(text_as_list[i])
+        text_as_list[i] = alphabet[original_position + shift]
+    
+    print("".join(text_as_list))
 
     ##HINT: How do you get the index of an item in a list:
     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
@@ -39,3 +32,5 @@ print(test2)
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+encrypt(text,shift)
