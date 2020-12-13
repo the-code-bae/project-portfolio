04facdd9d4801a8af14466880ee945e7ea5e1da1
makaibe <nibe0993@gmail.com>
2020-12-13 14:34:59 +0000

    First part completed

--- 100_days_of_code/day_10/10_2.py
+++ 100_days_of_code/day_10/10_2.py
@@ -9,3 +9,25 @@ def multiply(n1,n2):
 
 def divide(n1, n2):
 	return n1/n2
+
+operations =  {
+	"+": add,
+	"-": subtract,
+	"*": multiply,
+	"/": divide,
+}
+
+# function = operations["*"]
+# function(2,3)
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for symbol in operations:
+	print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+function = operations[operation_symbol]
+answer = function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
