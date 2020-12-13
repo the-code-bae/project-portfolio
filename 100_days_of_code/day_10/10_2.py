464b5369059f196cbfd3659ba35c2ab4bde5fb2a
makaibe <nibe0993@gmail.com>
2020-12-13 15:17:44 +0000

    Recursion solution

--- 100_days_of_code/day_10/10_2.py
+++ 100_days_of_code/day_10/10_2.py
@@ -1,3 +1,7 @@
+import art
+
+print(art.logo)
+
 def add (n1, n2):
 	return n1+n2
 
@@ -20,14 +24,30 @@ operations =  {
 # function = operations["*"]
 # function(2,3)
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
+def calculator():
 
-for symbol in operations:
-	print(symbol)
+	num1 = float(input("What's the first number?: "))
 
-operation_symbol = input("Pick an operation from the line above: ")
-function = operations[operation_symbol]
-answer = function(num1, num2)
+	for symbol in operations:
+		print(symbol)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+	should_continue = True
+
+	while should_continue:
+
+		operation_symbol = input("Pick an operation: ")
+
+		num2 = float(input("What's the next number?: "))
+
+		calculation_function = operations[operation_symbol]
+		answer = calculation_function(num1, num2)
+
+		print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+		
+		if input("Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.\n") == "y":
+			num1 = answer
+		else:
+			should_continue = False
+			calculator() # recursion 
+
