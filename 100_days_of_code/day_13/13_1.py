e41f1c4703345ce304c14dae0f693f3a836cb488
makaibe <nibe0993@gmail.com>
2020-12-22 00:08:01 +0000

    Added 5th solution

--- 100_days_of_code/day_13/13_1.py
+++ 100_days_of_code/day_13/13_1.py
@@ -20,18 +20,19 @@
 # elif year >= 1994:
 #   print("You are a Gen Z.")
 
-# Fix the Errors
-age = int(input("How old are you?"))
-if age > 18:
-    print("You can drive at age {age}.")
+# # Fix the Errors
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print("You can drive at age {age}.")
 
-# #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+print(word_per_page)
+total_words = pages * word_per_page
+print(total_words)
 
 # #Use a Debugger
 # def mutate(a_list):
