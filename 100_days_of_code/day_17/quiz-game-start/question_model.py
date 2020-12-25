ffca7df0210942c852a0bf817ab359be12c44654
makaibe <nibe0993@gmail.com>
2020-12-25 13:55:36 +0000

    Amended to include solution from lecture

--- 100_days_of_code/day_17/quiz-game-start/question_model.py
+++ 100_days_of_code/day_17/quiz-game-start/question_model.py
@@ -1,9 +1,9 @@
 class Question:
-    def __init__(self):
-        self.text = "2+3+5"
-        self.answer = "True"
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
 
 
-question_1 = Question()
+question_1 = Question("2+3+5", "True")
 print(question_1.text)
 print(question_1.answer)
