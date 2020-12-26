d1e2198faf0e2738b10ba35dc67a3808568538ce
makaibe <nibe0993@gmail.com>
2020-12-26 16:01:51 +0000

    Add quiz object show next question

--- 100_days_of_code/day_17/quiz-game-start/main.py
+++ 100_days_of_code/day_17/quiz-game-start/main.py
@@ -1,14 +1,11 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for q_data in question_data:
-    # print(dict["text"])
-    # print(dict["answer"])
     question_bank.append(Question(q_data["text"], q_data["answer"]))
 
-# print(len(question_bank))
-# print(question_bank[0].text)
-# print(question_bank[0].answer)
-
+quiz = QuizBrain(question_bank)
+quiz.next_question()
