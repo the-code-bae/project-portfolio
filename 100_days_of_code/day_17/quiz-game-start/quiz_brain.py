c8891438aa09993b42ac8a7a673d3442e57df597
makaibe <nibe0993@gmail.com>
2020-12-27 13:20:29 +0000

    Refactored still_has_questions()

--- 100_days_of_code/day_17/quiz-game-start/quiz_brain.py
+++ 100_days_of_code/day_17/quiz-game-start/quiz_brain.py
@@ -4,10 +4,7 @@ class QuizBrain:
         self.question_list = question_list
 
     def still_has_questions(self):
-        if self.question_number == len(self.question_list):
-            return False
-        else:
-            return True
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
