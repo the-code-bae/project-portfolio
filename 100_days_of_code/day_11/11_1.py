9e22748cf0f33eae1ac9b4d9a3f1ae962d1f8f59
makaibe <nibe0993@gmail.com>
2020-12-30 17:19:22 +0000

    Fix calculate score and add while loop

--- 100_days_of_code/day_11/11_1.py
+++ 100_days_of_code/day_11/11_1.py
@@ -51,7 +51,7 @@ def calculate_score(cards):
 
 	#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
-	if sum(cards) and len(cards) == 2:
+	if sum(cards) == 21 and len(cards) == 2:
 		return 0
 	#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
 	if 11 in cards and sum(cards) > 21:
@@ -69,18 +69,27 @@ for i in range(2):
 	user_cards.append(deal_card())
 	computer_cards.append(deal_card())
 
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-print(f"your cards: {user_cards}, current score : {user_score}")
-print(f"your cards: {computer_cards}, current score : {computer_score}")
+while not is_game_over:
+	#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+	user_score = calculate_score(user_cards)
+	computer_score = calculate_score(computer_cards)
+	print(f"your cards: {user_cards}, current score : {user_score}")
+	print(f"computer cards: {computer_cards}, current score : {computer_score}")
 
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-	is_game_over = True
+	if user_score == 0 or computer_score == 0 or user_score > 21:
+		is_game_over = True
+	else:
+		#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+		user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+		if user_should_deal == "y":
+			user_cards.append(deal_card())
+		else:
+			is_game_over = True
 
 
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+
+
 
 #Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
 
