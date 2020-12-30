526cf35a312322cda43e92171211aee1857ec2c3
makaibe <nibe0993@gmail.com>
2020-12-30 17:00:26 +0000

    Testing code

--- 100_days_of_code/day_11/11_1.py
+++ 100_days_of_code/day_11/11_1.py
@@ -34,14 +34,14 @@
 import random 
 
 start_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n':\n")
-print(start_game)
+# print(start_game)
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
 
 def deal_card():
 	cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    output = random.choice(cards)
-    return output
+	output = random.choice(cards)
+	return output
 
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input 
 #and returns the score. 
@@ -63,14 +63,22 @@ def calculate_score(cards):
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 for i in range(2):
 	user_cards.append(deal_card())
 	computer_cards.append(deal_card())
 
-calculate_score(user_cards)
-
 #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"your cards: {user_cards}, current score : {user_score}")
+print(f"your cards: {computer_cards}, current score : {computer_score}")
+
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+	is_game_over = True
+
 
 #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
 
