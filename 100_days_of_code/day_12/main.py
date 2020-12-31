b5895d5b1fd41f77b8187736df516153e71fa931
makaibe <nibe0993@gmail.com>
2020-12-31 16:33:16 +0000

    Added new file

---
+++ 100_days_of_code/day_12/main.py
@@ -0,0 +1,30 @@
+from random import randint
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+answer = randint(1,100)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100")
+
+def check_answer(guess, answer):
+	if guess > answer:
+		print ("Too high.")
+	elif guess < answer:
+		print("Too low.")
+	else:
+		print(f"You've got it! The answer was {answer}")
+
+def set_difficulty():
+	level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+	if level == 'easy':
+		return EASY_LEVEL_TURNS
+	else:
+		return HARD_LEVEL_TURNS
+
+
+guess = int(input("Make a guess: "))
+turns = set_difficulty
+
+print(f"You have {turns} attempts remaining to guess the number.")
+
+print(f"The correct answer is {answer}")
