c15d6f0aa5202e2b2eac575d996f0a0a6cffeeb8
Maka I <nibe0993@gmail.com>
2021-10-12 17:30:15 +0100

    Add script for waltham forest

---
+++ hmo_cleansing/waltham_forest/main.py
@@ -0,0 +1,71 @@
+import tabula
+import re
+import pandas as pd
+from pathlib import Path
+import datetime as dt
+import mi_property_analyser as mpa
+
+today_date = dt.datetime.today().strftime('%Y_%m_%d_%H%M%S')
+p = Path(__file__).parents[0]
+
+print(p)
+
+# Change pdf name
+df = tabula.read_pdf(p.joinpath("20201126 Waltham Forest Privated Public Register November 2020.pdf")
+                     , pages='all'
+                     , pandas_options = {'header': None})
+
+print('length of df:' + str(len(df)))
+
+# quit()
+
+class SplitFields:
+    def __init__(self, field):
+        self.field = field
+
+    @property
+    def split_list(self):
+        return re.split(r'([A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][A-Z]{2})', self.field)
+
+    @property
+    def property_address(self):
+        return ''.join(self.split_list[:len(self.split_list) - 1])
+
+    @property
+    def licence_holder(self):
+        return self.split_list[-1]
+
+
+complete_register = pd.concat(df, ignore_index=True)
+
+# remove first line which contains header data
+complete_register.drop([0], inplace=True)
+
+# reset index after dropping row
+complete_register.reset_index(drop=True, inplace=True)
+
+# get property address from field
+complete_register[5] = complete_register[1].apply(lambda x: SplitFields(x).property_address)
+
+# get licence holder from field
+complete_register[6] = complete_register.apply(lambda x: SplitFields(x[1]).licence_holder if pd.isnull(x[2]) else x[2], axis=1)
+
+# created a df with columns to keep
+final_df = complete_register[[0,5,6,3,4]].copy()
+
+# rename columns
+final_df.columns = ['ref_no', 'property_address', 'licence_holder', 'start_date', 'end_date']
+
+print(final_df.head())
+
+print('final df length: ' + str(len(final_df)))
+
+final_df = final_df[final_df['property_address'] != '']
+
+print('final df length: ' + str(len(final_df)))
+final_df['postcode'] = final_df['property_address'].map(mpa.NearestStation.extract_postcode)
+
+# final_df['nearest_station'] = final_df['property_address'].map(mpa.NearestStation.extract_postcode)
+# print(final_df['postcode'][:5].map(mpa.NearestStation.find_nearest_station))
+# final_df.to_csv(p.joinpath(today_date + "waltham_forest_prpl_register.csv"), index=False)
+
