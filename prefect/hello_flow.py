afaa19da822c42c52f4bc37c42bcf1ed4a0f296a
Maka I <nibe0993@gmail.com>
2022-01-16 12:34:25 +0000

    Change executor

--- prefect/hello_flow.py
+++ prefect/hello_flow.py
@@ -1,12 +1,16 @@
 import os
+import time
 
 import prefect
 from prefect import task, Flow, Parameter
 from prefect.run_configs import LocalRun
+from prefect.executors import LocalDaskExecutor
 
 
 @task
 def say_hello(name):
+    # Add a sleep to simulate some long-running task
+    time.sleep(10)
     # Load the greeting to use from an environment variable
     greeting = os.environ.get("GREETING")
     logger = prefect.context.get("logger")
@@ -19,6 +23,10 @@ with Flow("hello-flow") as flow:
 
 # Configure the `GREETING` environment variable for this flow
 flow.run_config = LocalRun(env={"GREETING": "Hello"})
+
+# Use a `LocalDaskExecutor` to run this flow
+# This will run tasks in a thread pool, allowing for parallel execution
+flow.executor = LocalDaskExecutor()
 
 # Register the flow under the "tutorial" project
 flow.register(project_name="tutorial")
