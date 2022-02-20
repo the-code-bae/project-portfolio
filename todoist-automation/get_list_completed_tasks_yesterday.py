8dc76ed00f8d3252e3892f14645c70eae24c8410
Maka I <nibe0993@gmail.com>
2022-02-20 14:04:16 +0000

    Reformat code into a class

--- todoist-automation/get_list_completed_tasks_yesterday.py
+++ todoist-automation/get_list_completed_tasks_yesterday.py
@@ -24,68 +24,73 @@ YESTERDAY_END = str(YESTERDAY) + "T23:59:59Z"
 
 # TODO create function to make sure date is in the correct format
 
-api = todoist.TodoistAPI(TODOIST_API_TOKEN)
-# Sync to API to get latest data
-api.sync()
+class TodoistConnector:
+    def api_client(self, api_token=None):
+        api_token = api_token or TODOIST_API_TOKEN
+        return todoist.TodoistAPI(api_token)
+
+    def has_values(self, dictionary):
+        # For each key in dictionary, check if keys in dict have values
+        bool_values = [any(dictionary[key]) for key in dictionary]
+
+        # Create a unique list of values
+        output = list(set(bool_values))
+
+        return output[0]
+
+    def get_completed_tasks(self, limit=None, start=None, end=None):
+        api = self.api_client()
+        api.sync  # Sync to API to get latest data
+        completed_tasks_data = []
+        limit = limit or 5
+        start = start or YESTERDAY_START
+        end = end or YESTERDAY_END
+        offset = 0
+        is_empty = 0
+        api_call = 0
+
+        while is_empty == 0:
+
+            data = api.completed.get_all(limit=limit
+                                         , offset=offset
+                                         , since=start
+                                         , until=end)
+
+            logging.info(f'API call: #{api_call + 1}, list count: {len(data)}, item count: {len(data["items"])}')
+            api_call += 1
+
+            if self.has_values(data):
+                completed_tasks_data.append(data)  # add items to list
+                offset += limit  # increase offset to get the next set of results
+            else:
+                is_empty += 1
+        return completed_tasks_data
+
+    def add_project_name(self, item, data):
+        project_id = str(item['project_id'])
+        item['project_name'] = data[project_id]['name']
+        return item
+
+    def convert_to_list_of_dicts(self, response_data):
+        output = []
+        for response in response_data:
+            print(response)
+            for j in response['items']:
+                print(j)
+                row = self.add_project_name(j, response['projects'])
+                output.append(row)
+                print(row)
+        return output
 
 
-def has_values(dictionary):
-    # For each key in dictionary, check if keys in dict have values
-    values = [any(dictionary[key]) for key in dictionary]
-
-    # Create a unique list of values
-    output = list(set(values))
-
-    return output[0]
-
-
-completed_tasks_data = []
-limit = 5
-offset = 0
-is_empty = 0
-api_call = 0
-
-while is_empty == 0:
-
-    data = api.completed.get_all(limit=limit
-                                 , offset=offset
-                                 , since=YESTERDAY_START
-                                 , until=YESTERDAY_END)
-
-    logging.info(f'API call: #{api_call + 1}, list count: {len(data)}, item count: {len(data["items"])}')
-    api_call += 1
-
-    if has_values(data):
-        completed_tasks_data.append(data)  # add items to list
-        offset += limit  # increase offset to get the next set of results
-    else:
-        is_empty += 1
-
-for line in completed_tasks_data:
-    print(line)
-
-
-def add_project_name(item, data):
-    project_id = str(item['project_id'])
-    # print(project_id)
-    item['project_name'] = data[project_id]['name']
-    return item
-
-
-def convert_to_list_of_dicts(response_data):
-    output = []
-    for response in response_data:
-        print(response)
-        for j in response['items']:
-            print(j)
-            row = add_project_name(j, response['projects'])
-            output.append(row)
-            print(row)
-    return output
-
-convert_to_list_of_dicts(completed_tasks_data)
+# convert_to_list_of_dicts(completed_tasks_data)
 # Create list of dictionaries for uploading to bigquery
 
 # Convert to json
 
 # Upload to bigquery
+
+if __name__ == '__main__':
+    t = TodoistConnector()
+    data = t.get_completed_tasks()
+    # t.convert_to_list_of_dicts(data)
