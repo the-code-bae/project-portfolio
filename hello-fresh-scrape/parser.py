64a59801af3e5e64531f96c398f4e4533ebf1ebe
Maka I <nibe0993@gmail.com>
2022-12-27 13:47:21 +0000

    Get nutrients and tidy script

--- hello-fresh-scrape/parser.py
+++ hello-fresh-scrape/parser.py
@@ -15,7 +15,7 @@ html_files = [f for f in HF_PATH.iterdir() if f.is_file() and f.suffix == ".html
 html_files.sort()
 
 data_dict = {}
-for f in html_files[:3]:
+for f in html_files[:1]:
     # print(f)
     # print(f.name)
     with open(f) as f_obj:
@@ -31,18 +31,38 @@ for f in html_files[:3]:
 
         # Get the ingredients
         for c in soup.find_all("span", class_="web-12wkbvj"):
-            # print(c.find_next(attrs={"data-test-id": "recipe-preview-ingredient-allergens"}))
-            # print(c)
-            # print(c.contents)
             if c.contents[0].name == "p":
-                # print(c.contents)
                 for i in c.contents[:2]:
                     if isinstance(i,str):
-                    # print(type(i))
-                    # if i.name == "p" or i.name is None:
-                        # print(i.name)
                         print(f"Food: {i}")
-                    # print(type(i))
                     if i.next.name not in ["span", "div"]:
                         print(f"Units: {i.next}")
+
+        # Get the nutrients
+
+        # Row headings
+        print("row headings")
+        nutrient_row_headings = soup.find_all("p", class_="web-9z87v7")
+        row_headings_flattened = []
+        for c in nutrient_row_headings[1:]:
+            # print(c.contents)
+            row_headings_flattened.extend(c.contents)
+        print(row_headings_flattened)
+
+
+        # per portion
+        print("per portion")
+        nutritional_values_per_portion = soup.find_all(attrs={"data-test-id": "nutritional-values-amount"})
+        per_portion_flattened = []
+        for c in nutritional_values_per_portion:
+            per_portion_flattened.extend(c.contents)
+        print(dict(zip(row_headings_flattened,per_portion_flattened)))
+
+        # per 100g
+        print("per 100g")
+        nutritional_values_per_100g = soup.find_all(attrs={"data-test-id": "nutritional-values-amount-100g"})
+        per_100g_flattened = []
+        for c in nutritional_values_per_100g:
+            per_100g_flattened.extend(c.contents)
+        print(dict(zip(row_headings_flattened,per_100g_flattened)))
 
